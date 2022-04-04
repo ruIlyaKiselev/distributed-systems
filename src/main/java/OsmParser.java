9dc202e25bbc548d91123dc985374e5c6b3857a8
ilyakiselev <ruilyakiselev>
2022-04-04 21:13:36 +0700

    fix namespace issue (just remove from .xsd file); replace stax processor to jaxb deserializer (unmarshaller)

--- src/main/java/OsmParser.java
+++ src/main/java/OsmParser.java
@@ -1,16 +1,10 @@
+import generated.Node;
+import generated.Tag;
 import org.apache.log4j.Logger;
-
-import javax.lang.model.element.QualifiedNameable;
-import javax.xml.namespace.QName;
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.stream.events.Attribute;
-import javax.xml.stream.events.StartElement;
-import javax.xml.stream.events.XMLEvent;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Iterator;
+import java.util.List;
 
 public class OsmParser {
 
@@ -30,49 +24,36 @@ public class OsmParser {
 
     private void process(InputStream inputStream, OsmProcessingResult osmProcessingResult) {
         try (StaxStreamProcessor processor = new StaxStreamProcessor(inputStream)) {
-            XMLEventReader xmlEventReader = processor.getReader();
-
             log.info("Start osm parsing");
 
-            while (xmlEventReader.hasNext()) {
+            while (true) {
+                Node node = processor.nextNode();
 
-                var currentEvent = xmlEventReader.nextEvent();
+                if (node == null) {
+                    break;
+                }
 
-                if (currentEvent.isStartElement()) {
-                    StartElement startElement = currentEvent.asStartElement();
+                processNode(node, osmProcessingResult);
+                List<Tag> tags = node.getTag();
 
-                    if (isNode(startElement)) {
-                        processNode(startElement, osmProcessingResult);
-                    }
-
-                    if (isTag(startElement)) {
-                        processTag(startElement, osmProcessingResult);
-                    }
+                for (int i = 0; i != tags.size(); i++) {
+                    processTag(tags.get(i), osmProcessingResult);
                 }
             }
 
-            xmlEventReader.close();
             log.info("Finish osm parsing");
         } catch (Exception e) {
             log.error(e);
         }
     }
 
-    private static void processNode(StartElement element, OsmProcessingResult osmProcessingResult) {
-        var user = element.getAttributeByName(new QName("user"));
-        osmProcessingResult.redactions.merge(user.getValue(), 1, Integer::sum);
+    private static void processNode(Node node, OsmProcessingResult osmProcessingResult) {
+        var user = node.getUser();
+        osmProcessingResult.redactions.merge(user, 1, Integer::sum);
     }
 
-    private static void processTag(StartElement element, OsmProcessingResult osmProcessingResult) {
-        var key = element.getAttributeByName(new QName("k"));
-        osmProcessingResult.tagKeys.merge(key.getValue(), 1, Integer::sum);
-    }
-
-    private boolean isNode(StartElement element) {
-        return element.getName().equals(new QName("node"));
-    }
-
-    private boolean isTag(StartElement element) {
-        return element.getName().equals(new QName("tag"));
+    private static void processTag(Tag tag, OsmProcessingResult osmProcessingResult) {
+        var key = tag.getK();
+        osmProcessingResult.tagKeys.merge(key, 1, Integer::sum);
     }
 }
--- src/main/java/StaxStreamProcessor.java
+++ src/main/java/StaxStreamProcessor.java
@@ -1,9 +1,14 @@
+import generated.Node;
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Unmarshaller;
 import org.apache.log4j.Logger;
 
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
 
 public class StaxStreamProcessor implements AutoCloseable {
@@ -11,14 +16,28 @@ public class StaxStreamProcessor implements AutoCloseable {
     private static final Logger log = Logger.getLogger(StaxStreamProcessor.class);
     private static final XMLInputFactory FACTORY = XMLInputFactory.newInstance();
     private final XMLEventReader reader;
+    private final Unmarshaller nodeUnmarshaller;
 
-    public StaxStreamProcessor(InputStream is) throws XMLStreamException {
+    public StaxStreamProcessor(InputStream is) throws XMLStreamException, JAXBException {
         reader = FACTORY.createXMLEventReader(is);
+        nodeUnmarshaller = JAXBContext.newInstance(Node.class).createUnmarshaller();
         log.info("Reader created");
     }
 
-    public XMLEventReader getReader() {
-        return reader;
+    public Node nextNode() throws XMLStreamException, JAXBException {
+        while (true) {
+            if (!reader.hasNext()) {
+                return null;
+            }
+            XMLEvent nextEvent = reader.peek();
+            if (nextEvent.isStartElement()) {
+                StartElement startElement = nextEvent.asStartElement();
+                if (startElement.getName().getLocalPart().equals("node")) {
+                    return (Node) nodeUnmarshaller.unmarshal(reader);
+                }
+            }
+            reader.nextEvent();
+        }
     }
 
     @Override
